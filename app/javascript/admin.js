af2a98f4223365f91f453d4c95df7c067ff7073c
Leonidas Amorim <devmaker.leonidas@gmail.com>
2022-10-12 11:51:35 -0300

    change folder apexcharts

--- app/javascript/admin.js
+++ app/javascript/admin.js
@@ -4,5 +4,4 @@ import "./controllers"
 import * as bootstrap from "bootstrap"
 //import './admin/js/jquery' 
 import './admin/js/app' 
-import './admin/js/apexcharts.min' 
 import './admin/js/pages/dashboard' 
