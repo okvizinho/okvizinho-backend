c7be3e5c5442d72267ace1db745da7dc36497c18
Leonidas Amorim <devmaker.leonidas@gmail.com>
2022-10-22 10:49:52 -0300

    add custom css

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,6 @@
 //= link_tree ../images
 //= link_tree ../builds
+//= link admin.scss
+//= link mazer-assets/css/pages/auth.css
+//= link mazer-assets/css/main/app.css
+//= link mazer-assets/css/shared/iconly.css
