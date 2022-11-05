c579afcaa8365e01071b582cff9f2fbe6d251ae1
Leonidas Amorim <devmaker.leonidas@gmail.com>
2022-11-05 10:45:04 -0300

    add model place rspec test

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -2,13 +2,13 @@
 
 require 'rails_helper'
 
-RSpec.describe Space, type: :model do
-  describe 'Create space' do
-    it 'create a space' do
-      space = FactoryBot.create(:space)
+RSpec.describe Place, type: :model do
+  describe 'Create place' do
+    it 'create a place' do
+      place = FactoryBot.create(:place)
 
-      expect(space).to be_valid
-      expect(space.cover_image_url).to be_present
+      expect(place).to be_valid
+      expect(place.cover_image_url).to be_present
     end
   end
 end
